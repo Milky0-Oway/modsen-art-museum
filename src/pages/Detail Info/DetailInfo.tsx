d7506be8cbfac244cae245cdb3bb9ba036e90a1a
Milky0-Oway <milka.korotkaya@gmail.com>
2024-07-31 23:22:35 +0300

    Configuration + Header

---
+++ src/pages/Detail Info/DetailInfo.tsx
@@ -0,0 +1,10 @@
+import { JSX } from 'react';
+const DetailInfo = (): JSX.Element => {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default DetailInfo;
---
+++ src/pages/Favorites/Favorites.tsx
@@ -0,0 +1,11 @@
+import { JSX } from 'react';
+import Header from "../../components/Header/Header.tsx";
+const Favorites = (): JSX.Element => {
+    return (
+        <div>
+            <Header isHomePage={false}/>
+        </div>
+    );
+}
+
+export default Favorites;
